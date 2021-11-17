5e116a354066cee917a311aa5a5f9cce8302b9bb
awma-Mi-Na <awmach.9@gmail.com>
2021-11-17 12:28:52 +0530

    comments feature update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
@@ -126,3 +127,5 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'create']);
